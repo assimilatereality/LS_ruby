fb946d9fc2fb114e789835f3ac0335d9c6ef9d2e
assimilatereality <assimilatereality@gmail.com>
2021-09-07 21:15:12 -0400

    rpsls updates

--- RB101/lesson_3/medium1_2.rb
+++ RB101/lesson_3/medium1_2.rb
@@ -1,3 +1,4 @@
 #puts "the value of 40 + 2 is " + (40 + 2)
 # fixed
-puts puts "the value of 40 + 2 is #{(40 + 2).to_s}"
+puts "the value of 40 + 2 is #{(40 + 2)}"
+puts "the value of 40 + 2 is " + (40 + 2).to_s
---
+++ RB101/lesson_3_again/easy2_5.rb
@@ -0,0 +1,4 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+flintstones_mod = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones == flintstones_mod
